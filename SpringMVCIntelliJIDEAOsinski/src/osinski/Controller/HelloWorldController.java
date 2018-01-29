d9f9ab082228539d7e87819bd4996f94cb0498f6
osinD <osinskipawel0@gmail.com>
2018-01-29 23:45:14 +0100

    nic

--- SpringMVCIntelliJIDEAOsinski/src/osinski/Controller/HelloWorldController.java
+++ SpringMVCIntelliJIDEAOsinski/src/osinski/Controller/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
-
+	//Controller
     @RequestMapping("/")
     public String helloWorld(){
         return "index";
